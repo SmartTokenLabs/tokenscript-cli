4ac10ac5b90a56d1f0cbe1f79d49045209a52f14
Michael Wallace <micwallace@gmx.com>
2022-09-10 18:59:35 +1000

    fix: schema conflicts & validation for multiple schema versions.

--- src/build/steps/validateXml.ts
+++ src/build/steps/validateXml.ts
@@ -9,17 +9,19 @@ export class ValidateXml implements IBuildStep {
 
 	async runBuildStep() {
 
-		this.context.updateStatus("Validate XML...");
+		this.context.updateStatus("Validate TSML...");
 
 		// TODO: Just keep the XML in memory and write it at the end
 		let xml = fs.readFileSync(this.context.workspace + BuildProcessor.OUTPUT_DIR + "/tokenscript.tsml");
 
+		const schemaVersion = xml.indexOf('xmlns:ts="http://tokenscript.org/2022/09/tokenscript"') > -1 ? "2022-09" : "2020-06";
+
 		try {
-			await validateXMLWithXSD(xml, __dirname + "/../../schema/2020-06/tokenscript.xsd");
+			await validateXMLWithXSD(xml, __dirname + "/../../schema/" + schemaVersion + "/tokenscript.xsd");
 
 		} catch (e: any){
-			let splitError = e.message.split("-:");
-			e.message = splitError[0] + "\r\n" + splitError[2].split("-")[0];
+			let splitError = e.message.split("<?xml");
+			e.message = splitError[0];
 			throw e;
 		}
 
