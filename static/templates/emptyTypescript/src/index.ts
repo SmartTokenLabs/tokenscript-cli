19413a3fa1c9113eb572aeab017723a9eaa46b1a
Michael Wallace <micwallace@gmx.com>
2023-11-09 14:29:58 +1100

    feat: New & updated templates WIP

--- static/templates/emptyTypescript/src/index.ts
+++ static/templates/emptyTypescript/src/index.ts
@@ -1,6 +1,40 @@
 
 function init(){
-	alert("Hello from tokenscript!");
+
+	// @ts-ignore
+	web3.tokens.dataChanged = (oldTokens, updatedTokens, cardId) => {
+		const currentTokenInstance = updatedTokens.currentInstance;
+
+		let content = "";
+
+		switch (document.location.hash){
+			case "#info":
+				content = renderInfo(currentTokenInstance);
+				break;
+			case "#mint":
+				content = renderMint(currentTokenInstance);
+				break;
+		}
+
+		document.getElementById("container").innerHTML = content;
+	};
+
+	function renderInfo(currentTokenInstance){
+		return `
+			<h3>Welcome to TokenScript</h3>
+			<p>When a card loads in TokenScript, it gets access to the token context data through TokenScript engine</p>
+			<pre>
+				${JSON.stringify(currentTokenInstance, null, 2)}
+			</pre>
+		`;
+	}
+
+	function renderMint(currentTokenInstance){
+		return `
+			<h3>Mint $tst{TOKENSCRIPT_NAME} ...</h3>
+			<p>Actions allow you to invoke a smart contract transaction or invoke a Javascript function within the view.</p>
+		`;
+	}
 }
 
 init();
